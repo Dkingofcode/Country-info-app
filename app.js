d71be69f15d991f9a8a9e4046991086550e6ebeb
Dkingofcode <doladepo128@gmail.com>
2023-01-25 12:01:25 +0000

    finished fetching the data from the api and passing it to the app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let searchBtn = document.getElementById("search-btn");
-
+let result =  document.getElementById("search-result")
 let countryInp = document.getElementById("country-inp");
+
 
 searchBtn.addEventListener("click", () => {
   let countryName = countryInp.value;
@@ -12,5 +13,53 @@ searchBtn.addEventListener("click", () => {
     console.log(data[0].flags.svg);
     console.log(data[0].name.common);
     console.log(data[0].continents[0]);
-  })    
+    console.log(Object.keys(data[0].currencies)[0]);
+    console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+    console.log(Object.values(data[0].languages).toString().split(",").join(", "));
+
+    result.innerHTML = ` <img src="${data[0].flags.svg}" class="flag-img">
+    <h2>${data[0].name.common}</h2>
+     <div class="wrapper">
+      <div class="data-wrapper">
+         <h4>Capital:</h4>
+         <span>${data[0].capital[0]}</span>
+     </div>  
+     </div>
+      <div class="wrapper">
+       <div class="data-wrapper">
+         <h4>Continent:</h4>
+         <span>${data[0].continents[0]}</span>
+         </div>
+         </div>
+
+         <div class="wrapper">
+       <div class="data-wrapper">
+         <h4>Population:</h4>
+         <span>${data[0].population}</span>
+         </div>
+         </div>
+
+         <div class="wrapper">
+       <div class="data-wrapper">
+         <h4>Currency:</h4>
+         <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}</span>
+         </div>
+         </div>
+
+         <div class="wrapper">
+       <div class="data-wrapper">
+         <h4>Common Languages:</h4>
+         <span>${Object.values(data[0].languages).toString().split(",").join(", ")}</span>
+         </div>
+         </div>
+     `;
+  })  
+  .catch(() => {
+    if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+    }
+    else{
+       result.innerHTML = `<h3>Please enter a valid country name`; 
+    }
+  })  
 })
